e708935077217dc16da1132558c28e037da31274
Hai iMac15 <caubenondo@gmail.com>
2022-04-30 14:01:53 -0700

    save before adding hourly temp

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,7 +25,7 @@ searchBtn.addEventListener("click", function (e) {
             `;
                 for (let city of data) {
                     templateHTML += ` 
-                    <button data-lat='${city.lat}' data-lon='${city.lon}' data-name='${city.name}' data-state='${!!city.state ? city.state : ""}' data-country='${city.country}' class='btn btn-outline-info btn-city mb-2 btn-block'>${city.name}, ${!!city.state ? city.state + "," : ""} ${city.country}</button>
+                    <button data-lat='${city.lat}' data-lon='${city.lon}' data-name='${city.name}' data-state='${!!city.state ? city.state : ""}' data-country='${city.country}' class='btn btn-outline-primary btn-city mb-2 btn-block'>${city.name}, ${!!city.state ? city.state + "," : ""} ${city.country}</button>
                 `;
                 }
                 templateHTML += `</div>`;
@@ -51,7 +51,6 @@ searchResult.addEventListener("click", function (e) {
         localStorage.setItem("cityHistory", JSON.stringify(localHistory));
         searchResult.innerHTML = "";
         searchInput.value = "";
-
         displayWeather(cityObj);
     }
 });
@@ -63,7 +62,7 @@ async function displayWeather(cityObj) {
         daKey;
     cityLabel.textContent = `${cityObj.name}, ${!!cityObj.state ? cityObj.state + "," : ""
         } ${cityObj.country}`;
-    forecastLabel.textContent = "Next 6 days";
+    forecastLabel.textContent = "6-Days Forecast";
     let hourly = [];
     await fetch(searchURI)
         .then((res) => res.json())
@@ -71,37 +70,63 @@ async function displayWeather(cityObj) {
             console.log(data);
 
             let templateHTML = `
-        <div class="card row">
-            <div class="card-body">
-                <h3 class="card-title">
-                ${moment(data.current.dt, "X").format("dddd, MMMM Do, YYYY")}
-                </h3>
-                <h6 class="card-subtitle mb-2 text-muted"></h6>
-                <div class='row'>
-                    <div class='col-md-4'><img class='card-img-top' src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png" class="rounded-circle card-img-top" alt="weather today"></div>
-                    <div class='col-md-4 align-self-center'>
-                    <p><i class="fa-solid fa-temperature-full"></i> ${data.current.temp} *F</p>
-                    <p><i class="fa-solid fa-wind"></i> ${data.current.wind_speed} MPH</p>
-                    <p><i class="fa-solid fa-droplet"></i> ${data.current.humidity}%</p>
-                    </div>
-                    <div class='col-md-4 align-self-center'>
-                    <p><i class="fa-solid fa-radiation ${data.current.uvi>=8?'text-danger':data.current.uvi>=5?'text-warning':data.current.uvi>=3?'text-info':'text-success'}"></i> ${data.current.uvi} (UVI)</p>
-                    <p><i class="fa-solid  fa-sun text-warning"></i> ${moment(data.current.sunrise, "X").format("h:mm A")}</p>
-                    <p><i class="fa-solid fa-sun text-info"></i> ${moment(data.current.sunset, "X").format("h:mm A")}</p>
+                <div class="card row">
+                    <div class="card-body">
+                        <h3 class="card-title">
+                        ${moment(data.current.dt, "X").format("dddd, MMMM Do, YYYY")}
+                        </h3>
+                        <h4 class="card-subtitle mb-2 text-muted">${data.current.weather[0].main} - ${data.current.weather[0].description}</h4>
+                        <div class='row'>
+                            <div class='col-md-4'><img class='card-img-top' src="https://openweathermap.org/img/wn/${data.current.weather[0].icon}@4x.png" class="rounded-circle card-img-top" alt="weather today"></div>
+                            <div class='col-md-4 align-self-center'>
+                            <p><i class="fa-solid fa-temperature-full ${data.current.temp>90?'text-danger':data.current.temp>70?'text-warning':data.current.temp>40?'text-success':data.current.temp>20?'text-info':'text-primrary'}"></i> ${data.current.temp} <span>&#176;</span>F</p>
+                            <p><i class="fa-solid fa-wind"></i> ${data.current.wind_speed} MPH</p>
+                            <p><i class="fa-solid fa-droplet"></i> ${data.current.humidity}%</p>
+                            </div>
+                            <div class='col-md-4 align-self-center'>
+                            <p><i class="fa-solid fa-radiation ${data.current.uvi>=8?'text-danger':data.current.uvi>=5?'text-warning':data.current.uvi>=3?'text-info':'text-success'}"></i> ${data.current.uvi} (UVI)</p>
+                            <p><i class="fa-solid  fa-sun text-warning"></i> ${moment(data.current.sunrise, "X").format("h:mma")}</p>
+                            <p><i class="fa-solid fa-sun text-info"></i> ${moment(data.current.sunset, "X").format("h:mma")}</p>
+                            </div>
+                            <canvas id='hourly'></canvas>
+                        </div>                
                     </div>
                 </div>
-                
-                
-                
-            </div>
-        </div>
-        
-        `;
+            `;
             mainCityDisplay.innerHTML = templateHTML;
             let forecastHTML = `<div class='row'>`;
             let temps = [];
             for (let i = 1; i <= 6; i++) {
                 const dayForecast = data.daily[i];
+
+
+
+                forecastHTML += `<div class='col-md-4 p-1' >
+                                <div class="card  p-0 ${dayForecast.temp.day>90?'border-danger':dayForecast.temp.day>70?'border-warning':dayForecast.temp.day>40?'border-success':dayForecast.temp.day>20?'border-info':'border-primrary'}">
+                                    <div class='card-header text-center bg-white'>
+                                    <h3 class="card-title col-12">${moment(dayForecast.dt, "X").format("dddd")}</h3>
+                                    <p class='text-center h5 text-muted'>${moment(dayForecast.dt, "X").format("DD-MM-YYYY")}</p>
+                                    </div>
+                                    <img class="card-img-top" src="https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@4x.png" alt="weather image">
+                                    <div class="card-body row">
+                                        <div class='col-12'>
+                                            <p class='h5 text-muted text-center'>${dayForecast.weather[0].main} - ${dayForecast.weather[0].description}</p>
+                                        </div>
+                                        <div class='col-6 col-md-12 col-lg-6'>
+                                            <p><i class="fa-solid fa-temperature-full ${dayForecast.temp.day>90?'text-danger':dayForecast.temp.day>70?'text-warning':dayForecast.temp.day>40?'text-success':dayForecast.temp.day>20?'text-info':'text-primrary'}"></i> ${dayForecast.temp.day} <span>&#176;</span>F</p>
+                                            <p><i class="fa-solid fa-wind"></i> ${dayForecast.wind_speed} MPH</p>
+                                            <p><i class="fa-solid fa-droplet"></i> ${dayForecast.humidity}%</p>
+                                        </div>
+                                        <div class='col-6 col-md-12 col-lg-6'>
+                                        <p><i class="fa-solid fa-radiation ${dayForecast.uvi>=8?'text-danger':dayForecast.uvi>=5?'text-warning':dayForecast.uvi>=3?'text-info':'text-success'}"></i> ${dayForecast.uvi}</p>
+                                        <p><i class="fa-solid  fa-sun text-warning"></i> ${moment(dayForecast.sunrise, "X").format("h:mma")}</p>
+                                        <p><i class="fa-solid fa-sun text-info"></i> ${moment(dayForecast.sunset, "X").format("h:mma")}</p>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                </div>
+                            `;
 
                 const temp = {
                     labels: ["morn", "day", "eve", "night"],
@@ -132,26 +157,6 @@ async function displayWeather(cityObj) {
                     ],
                 };
                 temps.push(temp);
-
-                forecastHTML += `
-            <div class="card col-md-4">
-                <img class="card-img-top" src="https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@4x.png" alt="weather image">
-                <div class="card-body">
-                <h4 class="card-title">${moment(dayForecast.dt, "X").format("ddd MM/DD/YY")}</h4>
-                <p>Temp: ${dayForecast.temp.day} *F</p>
-                    <p>Wind: ${dayForecast.wind_speed} MPH</p>
-                    <p>Humidity: ${dayForecast.humidity}%</p>
-                    <p>UV Index: ${dayForecast.uvi}</p>
-                    <p>Sunrise: ${moment(dayForecast.sunrise, "X").format(
-                        "h:mm A"
-                    )}</p>
-                    <p>Sunrise: ${moment(dayForecast.sunset, "X").format(
-                        "h:mm A"
-                    )}</p>
-                    <canvas id='tempDay-${i}'></canvas>
-                </div>
-            </div>
-          `;
             }
 
             forecastHTML += `</div>`;
@@ -168,7 +173,9 @@ async function displayWeather(cityObj) {
                     }
                 );
             }
-        });
+
+        
+    });
 }
 
 function displayHistory(cities) {
@@ -186,13 +193,15 @@ document.querySelector("#clear-btn").addEventListener("click", function (e) {
 history.addEventListener("click", function (e) {
     let target = e.target;
     if (target.matches(".btn-city-history")) {
-        const cityObj = {...target.dataset,};
+        const cityObj = {
+            ...target.dataset,
+        };
         displayWeather(cityObj);
     }
 });
 
 function init() {
-    localHistory =JSON.parse(localStorage.getItem("cityHistory")) || localHistory;
+    localHistory = JSON.parse(localStorage.getItem("cityHistory")) || localHistory;
     displayHistory(localHistory);
 }
 
